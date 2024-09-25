29ecb15c3160e85df28cbdb122622e309fe4152f
AhmedElsheekh <elsheekh418@gmail.com>
2024-09-25 13:19:37 +0300

    Add Main Files

---
+++ EvenOddNumbersApp/EvenOddNumbersApp/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace EvenOddNumbersApp
+{
+	internal class Program
+	{
+		static void PrintNumbers(string title, IEnumerable<int> numbers)
+		{
+            Console.Write($"{title}: [");
+			foreach (var n in numbers)
+				Console.Write($"{n},");
+            Console.Write("]");
+            Console.WriteLine();
+		}
+
+		static bool IsEven(int n)
+		{
+			if (n % 2 == 0)
+				return true;
+			else
+				return false;
+		}
+
+		static bool IsOdd(int n)
+		{
+			if (n % 2 != 0)
+				return true;
+			else
+				return false;
+		}
+
+		static void Main(string[] args)
+		{
+			int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			PrintNumbers("Numbers", nums);
+			PrintNumbers("Even No", nums.Where(x => IsEven(x)));
+			PrintNumbers("Odd No", nums.Where(x => IsOdd(x)));
+		}
+	}
+}
